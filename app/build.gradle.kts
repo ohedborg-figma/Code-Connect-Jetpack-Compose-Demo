dcba7c938771dd132789296df2540419e842d517
Ohedborg <oliver.hedborg@gmail.com>
2024-10-04 14:15:54 +0100

    jvmissue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("com.figma.code.connect") version "1.+"
+    id("com.figma.code.connect") version "1.1.4"
+    kotlin("jvm") version "2.0.0"
+
 }
 
 android {
@@ -30,19 +32,24 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "22" // Ensure this is correctly set
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.3" // Update as necessary
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,7 +58,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -68,12 +74,12 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // system UI Controller
+    // System UI Controller
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
 
     // Extended Icons
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
+    // Code Connect Library
     implementation("com.figma.code.connect:code-connect-lib:1.1.3")
-
 }
